d997857b509b7f24070d6fa9a4c5928b330cfa96
vishalsookram44 <vishalsookram44@gmail.com>
2026-04-23 00:48:19 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(SQLModel, table=True):
     password:str
 
     ## Task 3.1 code should go here (special care should go into the indentation)
-
+    todos: list['Todo'] = Relationship(back_populates="user")
     ## End of task 3.1 code
 
     def set_password(self, plaintext_password):
@@ -21,21 +21,29 @@ class User(SQLModel, table=True):
         return f"(User id={self.id}, username={self.username} ,email={self.email})"
 
 class TodoCategory(SQLModel, table=True):
-    # Implementation of the TodoCategory model from task 5.1 here
+    todo_id: int|None = Field(primary_key=True, foreign_key='todo.id')
+    category_id: int|None = Field(primary_key=True, foreign_key='category.id')
     pass
-
 
 class Todo(SQLModel, table=True):
-    ## Task 2.1 implementation here. Remove the line below that says "pass" once completed
-    pass
-
+    id: Optional[int] =  Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key='user.id') #set user_id as a foreign key to user.id 
+    text: str = Field(max_length=255)
+    done: bool = Field(default=False)
+    
+    # done: bool = False  # <---- can also be written this way if you prefer a pythonic default
+    def toggle(self):
+        self.done = not self.done
     ## Task 3.2 implementation should go here as well. Modify the class like you did for 3.1 above
-
-    ## Task 3.4 implementation should go here as well
+    user: User = Relationship(back_populates="todos")
 
     # Task 5.2 code should go here
-    
+    categories: list['Category'] = Relationship(back_populates=("todos"), link_model=TodoCategory)
     
 class Category(SQLModel, table=True):
-    # Implementation of the Category model from task 5.1 here
+    id: Optional[int] =  Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key='user.id') #set user_id as a foreign key to user.id 
+    text: str = Field(max_length=255)
+    todos: list['Todo'] = Relationship(back_populates=("categories"), link_model=TodoCategory)
+    
     pass
